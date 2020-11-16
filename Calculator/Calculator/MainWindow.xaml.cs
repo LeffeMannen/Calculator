6b4aa95806926d687dbc7b5832490bef8c6686ec
LeffeMannen <andreas.ohlsson@elev.ga.ntig.se>
2020-11-16 13:11:20 +0100

    jag commitade allt :D

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -20,9 +20,278 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        long number1 = 0;
+        long number2 = 0;
+        string operation = "";
+
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Proc_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "%";
+            textDisplay.Text = "%";
+        }
+        //fix later
+        private void ClearEntry_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = 0;
+            }
+            else
+            {
+                number2 = 0;
+            }
+            textDisplay.Text = "";
+        }
+        //fix later
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            number1 = 0;
+            number2 = 0;
+            operation = "";
+            textDisplay.Text = "";
+        }
+        //fix later
+        private void Backspace_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 / 10);
+                textDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 / 10);
+                textDisplay.Text = number2.ToString();
+            }
+        }
+        private void ParL_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "(";
+            textDisplay.Text = "(";
+        }
+        private void ParR_Click(object sender, RoutedEventArgs e)
+        {
+            operation = ")";
+            textDisplay.Text = ")";
+        }
+        private void E_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "e";
+            textDisplay.Text = "e";
+        }
+        private void Pi_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "π";
+            textDisplay.Text = "π";
+        }
+        private void X2_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "²";
+            textDisplay.Text = "²";
+        }
+        private void PosNeg_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 *= -1;
+                textDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 *= -1;
+                textDisplay.Text = number2.ToString();
+            }
+        }
+        private void Root_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "√";
+            textDisplay.Text = "√";
+        }
+        private void Div_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "/";
+            textDisplay.Text = "/";
+        }
+        private void Mult_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "*";
+            textDisplay.Text = "*";
+        }
+        private void Sub_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "-";
+            textDisplay.Text = "-";
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "+";
+            textDisplay.Text += "+";
+        }
+        //fix later
+        private void Coma_Click(object sender, RoutedEventArgs e)
+        {
+            operation = ",";
+            textDisplay.Text += ",";
+        }
+        //fix later
+        private void Equal_Click(object sender, RoutedEventArgs e)
+        {
+            textDisplay.Text += "="; 
+            switch (operation)
+            {
+                case "+":
+                    textDisplay.Text += (number1 + number2).ToString();
+                    break;
+                case "-":
+                    textDisplay.Text = (number1 - number2).ToString();
+                    break;
+                case "*":
+                    textDisplay.Text = (number1 * number2).ToString();
+                    break;
+                case "/":
+                    textDisplay.Text = (number1 / number2).ToString();
+                    break;
+            }
+        }
+        private void Zero_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10);
+                textDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10);
+                textDisplay.Text += number2.ToString();
+            }
+        }
+        private void One_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 1;
+                textDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 1;
+                textDisplay.Text += number2.ToString();
+            }
+            
+        }
+        private void Two_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 2;
+                textDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 2;
+                textDisplay.Text += number2.ToString();
+            }
+        }
+        private void Three_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 3;
+                textDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 3;
+                textDisplay.Text += number2.ToString();
+            }
+        }
+        private void Four_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 4;
+                textDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 4;
+                textDisplay.Text += number2.ToString();
+            }
+        }
+        private void Five_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 5;
+                textDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 5;
+                textDisplay.Text += number2.ToString();
+            }
+        }
+        private void Six_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 6;
+                textDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 6;
+                textDisplay.Text += number2.ToString();
+            }
+        }
+        private void Seven_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 7;
+                textDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 7;
+                textDisplay.Text += number2.ToString();
+            }
+        }
+        private void Eight_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 8;
+                textDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 8;
+                textDisplay.Text += number2.ToString();
+            }
+        }
+        private void Nine_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + 9;
+                textDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + 9;
+                textDisplay.Text += number2.ToString();
+            }
+        }
+
     }
 }
